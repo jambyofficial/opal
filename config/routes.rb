2f531ee2c42ccc4eac02001b5855425ef192fbfa
gunkadoodah <gary@jamby.co>
2014-05-19 17:20:09 -0700

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  root to: "statuses#index"
   resources :statuses
 
   # The priority is based upon order of creation: first created -> highest priority.
